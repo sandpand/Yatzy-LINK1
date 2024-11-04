cb88cf32d7d1490fa1f32c84ac68f3959de2fe0d
Eric Grahn <eric.grahn@chasacademy.se>
2024-11-04 15:15:29 +0100

    First commit

--- Yatzy.cpp
+++ Yatzy.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
 
-using namespace std; 
+using namespace std;
+
+bool gameIsRunning = true;
+
+void MainMenu()
+    {
+    int choice;
+    cin >> choice;
+    do {
+        switch (choice)
+        {
+        case 1:
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        default:
+        }
+        gameIsRunning = false;
+        } while (gameIsRunning);
+        cout << "Thanks for playing!";
+    }
+
+void Gameplay()
+{
+    string player1;
+    string player2;
+    //Greeting & asking player to enter their names;
+    cout << "Welcome to Yatzy";
+    cout << "Player 1, please enter your name";
+    cin >> player1;
+    cout << "Player 2, please enter your name";
+    cin >> player2;
+}
 
 int main ()
-{   
-    string player1; 
-    string player2; 
-
-    //Greeting & asking player to enter their names; 
-    cout << "Welcome to Yatzy";  
-
-    cout << "Player 1, please enter your name";    
-    cin >> player1; 
-
-    cout << "Player 2, please enter your name";    
-    cin >> player2; 
-
-
-
-    return 0; 
+{
+    MainMenu();
+    return 0;
 }
