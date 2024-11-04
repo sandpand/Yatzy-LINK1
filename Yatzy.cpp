0b42831b89e65d7935ec5cab1fda71822193b606
Sand <sandra.obradovic@chasacademy.se>
2024-11-04 13:51:34 +0100

    players can add their names

--- Yatzy.cpp
+++ Yatzy.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
-int main ()
-{
+using namespace std; 
 
-    
+int main ()
+{   
+    string player1; 
+    string player2; 
+
+    //Greeting & asking player to enter their names; 
+    cout << "Welcome to Yatzy";  
+
+    cout << "Player 1, please enter your name";    
+    cin >> player1; 
+
+    cout << "Player 2, please enter your name";    
+    cin >> player2; 
+
+
+
     return 0; 
 }
