cbb53952d29361e8efa67ad238ddb7b35d459c8b
Sand <sandra.obradovic@chasacademy.se>
2024-11-04 15:04:56 +0100

    Lets user roll 5dice --the values are not saved!!

--- Yatzy.cpp
+++ Yatzy.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <cstdlib> // Needed for rand() and srand()
+#include <ctime>   // Needed for time()
 
 using namespace std; 
 
-
+int diceRoll ();    
 
 int main ()
 {   
@@ -18,13 +20,18 @@ int main ()
     cout << "Player 2, please enter your name";    
     cin >> player2; 
 
-
-
-
-
-
-
-
-
     return 0; 
+}
+
+int diceRoll()
+{
+    // Set the seed using the current time
+    srand(static_cast<unsigned int>(time(0)));
+
+    // Generate and print 5 random numbers
+    for (int i = 0; i < 5; i++) 
+    {
+        int randomNumber = (rand() % 6) + 1; 
+        cout << randomNumber<< endl; // Random number between 0 and 99
+    }
 }
